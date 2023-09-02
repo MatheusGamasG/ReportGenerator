a3bac81ed5fdb039f71df973ec17483b922fa7b4
Matheus Guimarães <mgguima95@gmail.com>
2023-09-02 13:03:20 -0300

    Removing console logs

--- main.js
+++ main.js
@@ -13,13 +13,9 @@ app.on('ready', () => {
       preload: path.join(__dirname, 'preload.js')
     },
   });
-  console.log("Running!")
   mainWindow.loadFile('index.html');
-  console.log("File loaded!")
 
   ipcMain.on('button-clicked', (event, inputValue) => {
-    console.log("IPC Reading...");
-    console.log("Passed here " + inputValue.data);
     generate(inputValue.data);
   });
 });
